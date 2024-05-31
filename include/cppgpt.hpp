0e43e717baf53609c8b0b83d94b4d88f884d1d11
Ignacio Rodriguez <ignacionr@gmail.com>
2024-05-31 11:24:18 +0000

    chore: Refactor cppgpt constructor to include base_url parameter

--- include/cppgpt.hpp
+++ include/cppgpt.hpp
@@ -10,12 +10,8 @@ namespace ignacionr
 {
     class cppgpt
     {
-    private:
-        std::string api_key;            // Your OpenAI API key
-        std::vector<json> conversation; // To keep track of the conversation history
-
     public:
-        cppgpt(const std::string &api_key) : api_key(api_key) {}
+        cppgpt(const std::string &api_key, const std::string &base_url = "https://api.openai.com/v1/") : api_key_(api_key), base_url_{base_url} {}
 
         void add_instructions(std::string_view instructions, std::string_view role = "system")
         {
@@ -25,6 +21,7 @@ namespace ignacionr
         // Function to send a message to GPT and receive the reply
         json sendMessage(std::string_view message, std::string_view role = "user", std::string_view model = "gpt-3.5-turbo", float temperature = 0.25)
         {
+            std::cerr << "sendMessage: " << message << " with model " << model << std::endl;
             // Append the new message to the conversation history
             conversation.push_back({{"role", role}, {"content", message}});
 
@@ -35,12 +32,18 @@ namespace ignacionr
                 {"temperature", temperature}};
 
             // Send the API request
+            auto url = cpr::Url{base_url_ + "/chat/completions"};
+            std::cerr << "The URL is " << url << std::endl;
             auto r = cpr::Post(
-                cpr::Url{"https://api.openai.com/v1/chat/completions"},
-                cpr::Header{{"Authorization", "Bearer " + api_key}, {"Content-Type", "application/json"}},
+                url,
+                cpr::Header{{"Authorization", "Bearer " + api_key_}, {"Content-Type", "application/json"}},
                 cpr::Body{payload.dump()});
 
+            if (r.status_code != 200)
+                throw std::runtime_error("Error: " + r.text + " (" + std::to_string(r.status_code) + ")");
+
             // Parse the API response
+            std::cerr << "The response is " << r.text << " (" << r.text.size() << " bytes)" << std::endl;
             auto response = json::parse(r.text);
             auto gpt_reply = response["choices"][0]["message"]["content"];
 
@@ -54,5 +57,10 @@ namespace ignacionr
         {
             conversation.clear();
         }
+
+    private:
+        std::string api_key_;            // Your OpenAI API key
+        std::vector<json> conversation; // To keep track of the conversation history
+        std::string const base_url_;
     };
 }
